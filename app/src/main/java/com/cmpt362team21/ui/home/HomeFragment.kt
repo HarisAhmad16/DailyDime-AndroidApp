455b24234930c5e9a8ff366a609648c202b18749
HarisAhmad16 <harisahmad04@hotmail.com>
2023-12-02 13:59:14 -0800

    Chart working, not on update yet

--- app/src/main/java/com/cmpt362team21/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cmpt362team21/ui/home/HomeFragment.kt
@@ -1,17 +1,19 @@
 package com.cmpt362team21.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.anychart.AnyChart
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
 import com.cmpt362team21.R
 import com.cmpt362team21.databinding.FragmentHomeBinding
-import com.cmpt362team21.ui.income.IncomeItem
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlin.math.exp
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -19,14 +21,16 @@ class HomeFragment : Fragment() {
 
     private val homeViewModel: HomeViewModel = HomeViewModel.getInstance()
 
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
-        setupDatabaseListener()
+        dbListener()
 
         homeViewModel.firstName.observe(viewLifecycleOwner) { firstName ->
             binding.firstUserName.text = firstName
@@ -40,7 +44,7 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.nav_profile)
         }
 
-        return binding.root
+        return root
     }
 
     override fun onDestroyView() {
@@ -53,34 +57,77 @@ class HomeFragment : Fragment() {
             ?: throw IllegalStateException("Binding is null. Fragment is not attached or already destroyed.")
     }
 
-    private fun setupDatabaseListener() {
+    private fun dbListener() {
         val db = FirebaseFirestore.getInstance()
         val currentUser = FirebaseAuth.getInstance().currentUser
         val incomeCollection = db.collection("incomes")
+        val expenseCollection = db.collection("expenses")
+
+        var incomeBalance = 0.0
+        var expenseBalance = 0.0
+        var totalIncome = 0.0
+        var totalExpense = 0.0
 
         if (currentUser != null) {
             incomeCollection.whereEqualTo("userId", currentUser.uid)
                 .addSnapshotListener { snapshot, exception ->
                     if (exception != null) {
-                        Log.e("Firestore", "Listen failed.", exception)
                         return@addSnapshotListener
                     }
 
                     if (snapshot != null) {
-                        var currentBalance = 0.0
                         for (document in snapshot.documents) {
                             val amount = document.getDouble("amount") ?: 0.0
-                            currentBalance += amount
+                            incomeBalance += amount
                         }
+                        totalIncome = incomeBalance
 
-                        // Update UI on the main thread if fragment is still attached
                         if (isAdded) {
                             requireActivity().runOnUiThread {
-                                getFragmentBinding().incomeAmount.text = String.format("$%.2f", currentBalance)
+                                getFragmentBinding().incomeAmount.text = String.format("$%.2f", incomeBalance)
+                                updatePieChart(totalIncome, totalExpense)
+                            }
+                        }
+                    }
+                }
+
+            expenseCollection.whereEqualTo("userId", currentUser.uid)
+                .addSnapshotListener { snapshot, exception ->
+                    if (exception != null) {
+                        return@addSnapshotListener
+                    }
+
+                    if (snapshot != null) {
+                        for (document in snapshot.documents) {
+                            val amount = document.getDouble("amount") ?: 0.0
+                            expenseBalance += amount
+                        }
+                        totalExpense = expenseBalance
+
+                        if (isAdded) {
+                            requireActivity().runOnUiThread {
+                                getFragmentBinding().expensesAmount.text =
+                                    String.format("$%.2f", expenseBalance)
+                                updatePieChart(totalIncome, totalExpense)
                             }
                         }
                     }
                 }
         }
     }
+
+
+    private fun updatePieChart(totalIncome: Double, totalExpense: Double) {
+        val pie = AnyChart.pie()
+        val data: MutableList<DataEntry> = ArrayList()
+        data.add(ValueDataEntry("Income", totalIncome))
+        data.add(ValueDataEntry("Expenses", totalExpense))
+
+        pie.data(data)
+        pie.title("Income vs Expenses")
+
+        // Set up AnyChartView
+        binding.placeholder.setChart(pie)
+    }
 }
+
